b95451da5321ad73ed7ce5d56f974f06f8e6bc55
Costantino Cito <ccito.opendrako@gmail.com>
2015-06-15 09:45:39 +0200

    MDL-50537 chat: New WS mod_chat_get_chats_by_courses

--- lib/db/services.php
+++ lib/db/services.php
@@ -1055,6 +1055,14 @@ $functions = array(
         'type'          => 'read',
         'capabilities'  => 'moodle/rating:view'
     ),
+
+    'mod_chat_get_chats_by_courses' => array(
+        'classname'     => 'mod_chat_external',
+        'methodname'    => 'get_chats_by_courses',
+        'description'   => 'Retrieve a list of chats from several courses.',
+        'type'          => 'read',
+        'capabilities'  => ''
+    ),
 );
 
 $services = array(
@@ -1128,6 +1136,7 @@ $services = array(
             'gradereport_user_view_grade_report',
             'core_rating_get_item_ratings',
             'mod_url_view_url',
+            'mod_chat_get_chats_by_courses',
             ),
         'enabled' => 0,
         'restrictedusers' => 0,
